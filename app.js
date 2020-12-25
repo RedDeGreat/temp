ad1f273e444fbb8fdbeeb2bd9eb2d8d6646e8bb6
Katacoda Scenario <scenario@katacoda.com>
2020-12-25 23:33:12 +0000

    2nd commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+ const express = require("express");
+ const bodyParser = require('body-parser')
+
+ const app = express();
+ const port = 3000;
+
+ app.use(express.static('public'));
+ app.use(bodyParser.urlencoded({ extended: false }))
+
+ app.post("/",(req,res)=>{
+     console.log(req.body);
+     res.send("THX!");
+ })
+
+ app.listen(port,()=>console.log("App listening at port "+port));
